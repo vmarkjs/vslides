57e4316eb74876e9954bc9d4b353a73ca2d174c3
Yu Yin <yxonic@gmail.com>
2022-11-04 11:33:06 +0800

    feat: prettier, eslint and husky

---
+++ packages/vite-plugin/src/index.ts
@@ -0,0 +1,13 @@
+import { Plugin } from 'vite'
+
+export default function vitepPlugin(): Plugin {
+  return {
+    name: 'vslides',
+    transform() {
+      // TODO
+    },
+    configureServer() {
+      // TODO
+    },
+  }
+}
