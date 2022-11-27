3ea44792ab19033863a2d274cfaf45cefde6aaa8
Yu Yin <yxonic@gmail.com>
2022-11-28 01:21:28 +0800

    refactor: base style

--- packages/vite-plugin/src/index.ts
+++ packages/vite-plugin/src/index.ts
@@ -27,16 +27,16 @@ export default function vitePlugin(option: VSlidesPluginOption): Plugin {
       let code = `\n\nimport { h, reactive } from "vue";`
       code += `\nimport Presenter from "@vslides/core/components/Presenter.vue";`
       code += '\nimport "@vslides/core/style/base.css"'
-      code += `\nexport const nodes = reactive([]);`
+      code += `\nexport const pages = reactive([]);`
       await Promise.all(
         src.split('---').map(async (s) => {
           const { text } = await renderer.render(s)
-          code += `\nnodes.push(${text});`
+          code += `\npages.push(h('div', {class: 'vslides-page-content'}, ${text}));`
         }),
       )
 
       // handle hmr
-      code += `\nconst _default = { setup() { return () => h(Presenter, { pages: nodes }) } };`
+      code += `\nconst _default = { setup() { return () => h(Presenter, { pages }) } };`
       code += `\n_default.__hmrId = '${hash(id)}';`
       code += `\n_default.__file = '${id}';`
       code += `\n__VUE_HMR_RUNTIME__.createRecord(_default.__hmrId, _default);`
