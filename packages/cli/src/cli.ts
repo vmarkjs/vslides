d32515911f80e72af7b040ac56a1349d0202e4f5
Yu Yin <yxonic@gmail.com>
2022-12-07 17:02:23 +0800

    fix: find correct node_modules

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,4 +1,6 @@
+import fs from 'node:fs'
 import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import cac from 'cac'
 import { createServer } from 'vite'
 import vslidesPlugin from '@vslides/vite-plugin'
@@ -6,17 +8,32 @@ import vue from '@vitejs/plugin-vue'
 import unocss from 'unocss/vite'
 
 async function serve(filename: string) {
-  const filepath = path.resolve(filename)
+  const sourcePath = fileURLToPath(import.meta.url)
+  const filePath = path.resolve(filename)
+  const fileDir = path.resolve(path.dirname(filename))
+
+  const modulePath = fs.existsSync(
+    path.resolve(sourcePath, '../../node_modules/'),
+  )
+    ? path.resolve(sourcePath, '../../node_modules/')
+    : path.resolve(sourcePath, '../../../')
+
+  const rootPath = path.resolve(
+    process.env.NODE_PATH ? process.env.NODE_PATH.split(':')[0] : modulePath,
+  )
+
   const server = await createServer({
+    root: rootPath,
     optimizeDeps: {
-      disabled: true,
+      include: ['vue'],
     },
     define: {
       __VUE_OPTIONS_API__: false,
       __VUE_PROD_DEVTOOLS__: false,
     },
-    plugins: [vslidesPlugin({ serveFile: filepath }), vue(), unocss()],
+    plugins: [vslidesPlugin({ serveFile: filePath }), vue(), unocss()],
   })
+  server.watcher.add(fileDir)
   server.listen()
 }
 
