81994f869f9ac740c77864ae0a8141feef8ba73d
Yu Yin <yxonic@gmail.com>
2022-12-08 01:44:24 +0800

    fix: remove support for global installation

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,6 +1,4 @@
-import fs from 'node:fs'
 import path from 'node:path'
-import { fileURLToPath } from 'node:url'
 import cac from 'cac'
 import { createServer } from 'vite'
 import vslidesPlugin from '@vslides/vite-plugin'
@@ -8,29 +6,10 @@ import vue from '@vitejs/plugin-vue'
 import unocss from 'unocss/vite'
 
 async function serve(filename: string) {
-  const sourcePath = fileURLToPath(import.meta.url)
   const filePath = path.resolve(filename)
-  const fileDir = path.resolve(path.dirname(filename))
-
-  const modulePath = fs.existsSync(
-    path.resolve(sourcePath, '../../node_modules/'),
-  )
-    ? path.resolve(sourcePath, '../../node_modules/')
-    : path.resolve(sourcePath, '../../../')
-
-  const rootPath = path.resolve(
-    process.env.NODE_PATH ? process.env.NODE_PATH.split(':')[0] : modulePath,
-  )
-
   const server = await createServer({
-    root: rootPath,
     optimizeDeps: {
       include: ['vue'],
-    },
-    resolve: {
-      alias: {
-        '@vslides/core': '@vslides/core',
-      },
     },
     define: {
       __VUE_OPTIONS_API__: false,
@@ -38,12 +17,10 @@ async function serve(filename: string) {
     },
     plugins: [vslidesPlugin({ serveFile: filePath }), vue(), unocss()],
   })
-  server.watcher.add(fileDir)
   server.listen()
 }
 
 const cli = cac('vslides')
-
 cli.command('<path>', `source file`).action(serve)
 cli.help()
 cli.parse()
